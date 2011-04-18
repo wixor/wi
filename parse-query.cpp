b0790a6b06a8419e3908da35c0b391ddb1ea226a
wixor <wixorpeek@gmail.com>
2011-04-18 10:59:20 +0200

    parse-query: introducing TermReader

--- parse-query.cpp
+++ parse-query.cpp
@@ -2,174 +2,199 @@
 #include "bufrw.h"
 #include "parse-query.h"
 
+bool TermReader::isWhitespace(int c)
+{
+    static const char stopchars[] =
+        " \n\t\r`~!@#$%^&*-_=+\[]{};':,./<>?";
+    for(const char *p = stopchars; *p; p++)
+        if(c == *p)
+            return true;
+    return false;
+}
+bool TermReader::isTermSeparator(int c) {
+    return isWhitespace(c) || c == '|' || c == ')' || c == '(' || c == '"';
+}
+
+bool TermReader::eatSymbol(int sym)
+{
+    size_t start = tell();
+
+    while(!eof())
+    {
+        int c = read_utf8();
+        if(c == sym)
+            return true;
+        if(!isWhitespace(c))
+            break;
+    }
+
+    seek(start);
+    return false;
+}
+
+int TermReader::eatWhitespace()
+{
+    int count = 0;
+    while(!eof()) {
+        size_t where = tell();
+        int c = read_utf8();
+        if(!isWhitespace(c)) {
+            seek(where);
+            break;
+        }
+        count++;
+    }
+    return count;
+}
+
+char *TermReader::readTerm(void *memctx)
+{
+    eatWhitespace();
+
+    size_t start = tell(), end = start;
+    while(!eof()) {
+        end = tell();
+        int c = read_utf8();
+        if(isTermSeparator(c))
+            break;
+    }
+    seek(end);
+
+    if(start == end)
+        return NULL;
+
+    size_t len = end-start;
+    char *term = (char *)talloc_size(memctx, len+1);
+    assert(term);
+
+    rd.read_raw_at(start, term, len);
+    term[len] = '\0';
+
+    return term;
+}
+
+/* -------------------------------------------------------------------------- */
+
 class QueryParser
 {
     void *memctx;
-    Reader rd;
+    TermReader trd;
 
-    QueryNode *makeNode(QueryNode::Type type, QueryNode *lhs, QueryNode *rhs, const char *term)
-    {
-        QueryNode *node = talloc(memctx, QueryNode);
-        assert(node);
-        node->type = type;
-        node->rhs = rhs;
-        node->lhs = lhs;
-        node->term = term;
-        if(rhs) talloc_steal(node, rhs);
-        if(lhs) talloc_steal(node, lhs);
-        if(term) talloc_steal(node, term);
-        return node;
-    }
-    QueryNode *makeOpNode(QueryNode::Type type, QueryNode *lhs, QueryNode *rhs)
-    {
-        return makeNode(type, lhs, rhs, NULL);
-    }
-    QueryNode *makeTermNode(size_t offs, size_t len)
-    {
-        char *termcpy = (char *)talloc_size(memctx, len+1);
-        assert(termcpy);
-        rd.read_raw_at(offs, termcpy, len);
-        termcpy[len] = '\0';
-        return makeNode(QueryNode::TERM, NULL, NULL, termcpy);
-    }
+    QueryNode *makeNode(QueryNode::Type type, QueryNode *lhs, QueryNode *rhs, const char *term) const;
 
-    static inline bool isWhitespace(int c) {
-        return c == ' ' || c == '\n' || c == '\t';
-    }
-    static inline bool isTermSeparator(int c) {
-        return  isWhitespace(c) || c == '|' || c == ')' || c == '(' || c == '"';
-    }
-    bool eatSymbol(int sym)
-    {
-        size_t start = rd.tell();
-
-        while(!rd.eof())
-        {
-            int c = rd.read_utf8();
-            if(c == sym)
-                return true;
-            if(!isWhitespace(c))
-                break;
-        }
-
-        rd.seek(start);
-        return false;
-    }
-    void eatWhitespace()
-    {
-        while(!rd.eof()) {
-            size_t where = rd.tell();
-            int c = rd.read_utf8();
-            if(!isWhitespace(c)) {
-                rd.seek(where);
-                break;
-            }
-        }
-    }
-
-    QueryNode *parseTerm()
-    {
-        eatWhitespace();
-
-        size_t start = rd.tell(), end = start;
-        while(!rd.eof()) {
-            end = rd.tell();
-            int c = rd.read_utf8();
-            if(isTermSeparator(c))
-                break;
-        }
-        rd.seek(end);
-
-        return start == end ? NULL : makeTermNode(start, end-start);
-    }
-
-    QueryNode *parse0() /* parentheses + terms */
-    {
-        if(eatSymbol('(')) {
-            QueryNode *inside = parse2();
-            if(!eatSymbol(')'))
-                throw "unmatched parentheses";
-            return inside;
-        }
-        return parseTerm();
-    }
-
-    QueryNode *parse1() /* or-s */
-    {
-        QueryNode *lhs = parse0();
-        if(!eatSymbol('|'))
-            return lhs;
-        QueryNode *rhs = parse1();
-        if(!rhs)
-            throw "no rhs for '|'";
-        return makeOpNode(QueryNode::OR, lhs, rhs);
-    }
-
-    QueryNode *parse2() /* and-s */
-    {
-        QueryNode *lhs = parse1();
-        if(!eatSymbol(' '))
-            return lhs;
-        QueryNode *rhs = parse2();
-        if(!rhs)
-            return lhs;
-        return makeOpNode(QueryNode::AND, lhs, rhs);
-    }
-
-    QueryNode *parsePhrase() /* "query" */
-    {
-        if(!eatSymbol('"'))
-            return NULL;
-
-        QueryNode *root = makeOpNode(QueryNode::PHRASE, NULL, NULL);
-
-        QueryNode *last = root;
-        while(!rd.eof()) {
-            QueryNode *term = parseTerm();
-            if(!term) break;
-            talloc_steal(last, term);
-            last->rhs = term;
-            last = term;
-        }
-
-        if(!eatSymbol('"'))
-            throw "unmatched quotes";
-
-        return root;
-    }
-
-    QueryNode *parseQuery()
-    {
-        QueryNode *root = NULL;
-        if(!root) root = parsePhrase();
-        if(!root) root = parse2();
-        eatWhitespace();
-
-        if(rd.eof()) return root;
-        throw "garbage at end";
-    }
-
+    QueryNode *parse0(); /* parentheses + terms */;
+    QueryNode *parse1(); /* or-s */
+    QueryNode *parse2(); /* and-s */
+    QueryNode *parsePhrase(); /* "query" */
+    QueryNode *parseQuery();
 public:
-    QueryNode *run(const char *query)
-    {
-        memctx = talloc_new(NULL);
-        rd.attach(query, strlen(query));
-        QueryNode *root = NULL;
-
-        try {
-            root = parseQuery();
-        } catch(const char *err) {
-            fprintf(stderr, "malformed query: %s\n", err);
-        }
-
-        if(root) {
-            talloc_steal(NULL, root);
-            assert(talloc_total_size(memctx) == 0);
-        }
-        talloc_free(memctx);
-        return root;
-    }
+    QueryNode *run(const char *query);
 };
+
+QueryNode *QueryParser::makeNode(QueryNode::Type type, QueryNode *lhs, QueryNode *rhs, const char *term) const
+{
+    QueryNode *node = talloc(memctx, QueryNode);
+    assert(node);
+    node->type = type;
+    node->rhs = rhs;
+    node->lhs = lhs;
+    node->term = term;
+    if(rhs) talloc_steal(node, rhs);
+    if(lhs) talloc_steal(node, lhs);
+    if(term) talloc_steal(node, term);
+    return node;
+}
+
+QueryNode *QueryParser::parse0() /* parentheses + terms */
+{
+    if(trd.eatSymbol('(')) {
+        QueryNode *inside = parse2();
+        if(!trd.eatSymbol(')'))
+            throw "unmatched parentheses";
+        return inside;
+    }
+    char *term = trd.readTerm(memctx);
+    return !term ? NULL : makeNode(QueryNode::TERM, NULL, NULL, term);
+}
+
+QueryNode *QueryParser::parse1() /* or-s */
+{
+    QueryNode *lhs = parse0();
+    if(!trd.eatSymbol('|'))
+        return lhs;
+    QueryNode *rhs = parse1();
+    if(!rhs)
+        throw "no rhs for '|'";
+    return makeNode(QueryNode::OR, lhs, rhs, NULL);
+}
+
+QueryNode *QueryParser::parse2() /* and-s */
+{
+    QueryNode *lhs = parse1();
+    if(!trd.eatWhitespace())
+        return lhs;
+    QueryNode *rhs = parse2();
+    if(!rhs)
+        return lhs;
+    return makeNode(QueryNode::AND, lhs, rhs, NULL);
+}
+
+QueryNode *QueryParser::parsePhrase() /* "query" */
+{
+    if(!trd.eatSymbol('"'))
+        return NULL;
+
+    QueryNode *root = makeNode(QueryNode::PHRASE, NULL, NULL, NULL);
+
+    QueryNode *last = root;
+    while(!trd.eof())
+    {
+        char *tstr = trd.readTerm(memctx);
+        if(!tstr) break;
+
+        QueryNode *term = makeNode(QueryNode::TERM, NULL, NULL, tstr);
+        talloc_steal(last, term);
+
+        last->rhs = term;
+        last = term;
+    }
+
+    if(!trd.eatSymbol('"'))
+        throw "unmatched quotes";
+
+    return root;
+}
+
+QueryNode *QueryParser::parseQuery()
+{
+    QueryNode *root = NULL;
+    if(!root) root = parsePhrase();
+    if(!root) root = parse2();
+    trd.eatWhitespace();
+
+    if(trd.eof()) return root;
+    throw "garbage at end";
+}
+
+QueryNode *QueryParser::run(const char *query)
+{
+    memctx = talloc_new(NULL);
+    trd.attach(query, strlen(query));
+    QueryNode *root = NULL;
+
+    try {
+        root = parseQuery();
+    } catch(const char *err) {
+        fprintf(stderr, "malformed query: %s\n", err);
+    }
+
+    if(root) {
+        talloc_steal(NULL, root);
+        assert(talloc_total_size(memctx) == 0);
+    }
+    talloc_free(memctx);
+    return root;
+}
 
 struct QueryNode* parse_query(const char *query)
 {
--- parse-query.h
+++ parse-query.h
@@ -1,6 +1,53 @@
 #ifndef __PARSE_QUERY_H__
 #define __PARSE_QUERY_H__
 
+#include <stdlib.h>
+#include "bufrw.h"
+
+/*
+ * TermReader: read a stream of terms from memory buffer
+ *
+ * TermReader is a built upon Reader and allows extraction of consecutive terms
+ * from given memory buffer. Some of underying Reader's API is exposed.
+ * Also requirements for buffer are those of Reader.
+ *
+ * This facility is used for query parsing, so some characters are considered
+ * special. Namely, round parentheses and double quote symbol are term
+ * separators, but are not whitespace. see isWhitespace vs isTermSeparator.
+ *
+ * API summary:
+ * - attach, seek, tell, eof, read_utf8: as in Reader
+ * - eatSymbol: read given symbol from stream, possibly preceded with some
+ *      whitespace. if symbol is not found, do nothing (stream position is
+ *      not changed)
+ * - eatWhitespace: read all whitespace from stream up to first other symbol.
+ *      returns number of symbols read.
+ * - readTerm: read one term, possibly preceded with some whitespace, create
+ *      its copy and return it. stream position is placed just after last
+ *      symbol read. memory for copy is allocated with talloc from given memctx.
+ */
+class TermReader
+{
+    Reader rd;
+
+    static inline bool isWhitespace(int c);
+    static inline bool isTermSeparator(int c);
+
+public:
+    inline void attach(const void *buf, size_t len) {
+        rd.attach(buf, len);
+    }
+    inline void seek(ssize_t offs, int whence = SEEK_SET) { return rd.seek(offs, whence); }
+    inline size_t tell() const { return rd.tell(); }
+    inline bool eof() const { return rd.eof(); }
+
+    inline int read_utf8() { return rd.read_utf8(); }
+
+    bool eatSymbol(int c);
+    int eatWhitespace();
+    char *readTerm(void *memctx = NULL);
+};
+
 struct QueryNode {
     enum Type { AND, OR, PHRASE, TERM } type;
     QueryNode *rhs, *lhs;
