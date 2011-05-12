a850afa78aa8d9bc48cb529abcfd45af6f9b0ac5
Mateusz Dereniowski <derenio@gmail.com>
2011-05-12 11:39:02 +0200

    tokenize new regex's

--- tokenize.py
+++ tokenize.py
@@ -4,59 +4,59 @@
 # Mateusz Dereniowski
 # Wyszukiwanie informacji lato 2011
 
-import sys, os, codecs
+import sys, os
 from struct import pack
 import re
 import string
+try:
+	import tools
+	dummy_word = tools.dummy_word
+except ImportError:
+	dummy_word = r'"'
 
 def tokenize(filename):
 	"""Tokenizing given text,\nTitles starts with tag: "##TITLE## "."""
 
-	dummy_word = '"'
 	title_pattern = re.compile(r"^##TITLE## (.*)$")
-	diacritics = r"ąęśćżźńółèüäéáúūōíñõ"# ąęśćżźńółÀ-öø-ÿĀ-ſ
-	unwanted_char = re.compile(r"[^- a-z0-9{0}_.']".format(diacritics))
-	symbols = r"""[,'"!?:;@#$%^&*()+=[\]{}\|<>/]"""
-	unwanted_symbols = re.compile(r"(?:{0})|(?:\B[-.']\B)|(?:(?<=\d)[-'.](?=\d))|(?:\B[-.']\b)|(?:\b[-.']\B)".format(symbols))
-	parenthesis = re.compile(r"[()]")
-	spaces = re.compile(r"\s+")
-	only_spaces = re.compile(r"^\s+$")
-	#lower = string.maketrans(r"ABCDEFGHIJKLMNOPQRSTUVWXYZĄĘŚĆŻŹŃÓŁÈÜÄÉÁÚŪŌÍÑÕ", r"abcdefghijklmnopqrstuvwxyząęśćżźńółèüäéáúūōíñõ")
+	word = ur"""[a-zß-öø-ÿĀ-ſ]"""
+	not_word = ur"""[^a-zß-öø-ÿĀ-ſ]""" # ąęśćżźńółèüäéáúūōíñõ
+	unwanted_char = re.compile(ur"{0}[^-0-9.']".format(not_word), re.U)
+	symbols = ur"""[,"!?:;@#$%^&*()+_=[\]{}\|<>/]"""
+	unwanted_symbol = re.compile(ur"(?:{symbols})|(?:(?<={not_word})[-.'])|(?:[-.'](?={not_word}))|(?:^[-.'])|(?:[-.']$)".format(symbols=symbols, not_word=not_word), re.U)
+	parenthesis = re.compile(ur"[()]", re.U)
 
 	tytuly_count = 0
 	### reading and parsing file ###
-	with open(filename, 'r', buffering=2**27) as f_SRC:
-	 with open('TITL-'+os.path.basename(filename), 'w', buffering=2**25) as f_TITL:
-	  with open('TOKENS-'+os.path.basename(filename), 'w', buffering=2**25) as f_TOKENS:
+	with open(filename, 'rb', buffering=2**27) as f_SRC:
+	 with open('db/TITL-'+os.path.basename(filename), 'wb', buffering=2**25) as f_TITL:
+	  with open('db/TOKENS-'+os.path.basename(filename), 'wb', buffering=2**25) as f_TOKENS:
 		f_TITL.write('TITL')
 		f_TITL.seek(4, 1)
-		i = 0
+
 		for line in f_SRC:
+			line = line.decode('utf8')
 			title = title_pattern.match(line)
 			if title:
 				if tytuly_count != 0:
 					f_TOKENS.write('\n')
 				tytuly_count += 1
 				tytul = title.group(1).strip()
-				f_TITL.write(tytul + "\x00")
-				#tytul = tytul.translate(lower)
-				tytul = tytul.decode('utf8').lower().encode('utf8')
-				tytul = re.sub(parenthesis, r"", tytul)
-				f_TOKENS.write(' '.join((tytul, dummy_word)))
+				f_TITL.write(tytul.encode('utf8') + "\x00")
+				tytul = tytul.lower()
+				tytul = re.sub(parenthesis, ur"", tytul)
+				f_TOKENS.write(' '.join((tytul.encode('utf8'), dummy_word)))
 			else:
-				#line = line.translate(lower)
-				line = line.decode('utf8').lower().encode('utf8')
-				line = re.sub(unwanted_symbols, r" ", line)
-				if not re.match(only_spaces, line):
-					tokens = re.split(spaces, line.strip())
-					for token in tokens:
-						if unwanted_char.search(token):
-							f_TOKENS.write(' ' + dummy_word)
-						else:
-							f_TOKENS.write(' ' + token)
+				line = line.lower()
+				line = re.sub(unwanted_symbol, ur" ", line)
+				tokens = line.split()
+				for token in tokens:
+					if unwanted_char.search(token):
+						f_TOKENS.write(' ' + dummy_word)
+					else:
+						f_TOKENS.write(' ' + token.encode('utf8'))
 		f_TOKENS.write('\n')
 
-	 with open('TITL-'+os.path.basename(filename), 'rb+') as f_TITL:
+	 with open('db/TITL-'+os.path.basename(filename), 'rb+') as f_TITL:
 		f_TITL.seek(4, 0)
 		f_TITL.write(pack('<I', tytuly_count))
 
