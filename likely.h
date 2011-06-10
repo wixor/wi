b28913d57a3cc4ac79f9e7b9bc5c0fc2c8bfd3ae
wixor <wixorpeek@gmail.com>
2011-06-10 15:45:52 +0200

    likely.h: add compile_time_assert

--- likely.h
+++ likely.h
@@ -6,3 +6,11 @@
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #endif
 
+#ifndef compile_time_assert
+
+#define __cta2(x,line) static int __cta_ ## line [ !!(x) - 1 ] __attribute__((unused))
+#define __cta(x,line) __cta2(x,line)
+#define compile_time_assert(x) __cta(x,__LINE__)
+
+#endif
+
